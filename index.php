bc0876d4f576404a645548f45a28dfc22fb8b5d4
Henrique Pappis <henrique.pappis@simers.org.br>
2017-07-10 09:15:08 -0300

    gitignore e index

---
+++ index.php
@@ -0,0 +1,13 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
+		</div>
+		<div class="col-md-4">
+			 <span class="label label-default">Label</span> <span class="label label-default">Label</span>
+		</div>
+		<div class="col-md-4">
+			 <span class="label label-default">Label</span> <span class="label label-default">Label</span> <span class="label label-default">Label</span>
+		</div>
+	</div>
+</div>
